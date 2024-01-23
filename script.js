d8bb925d753ee9ac0ddb461c9d8747c50b04b754
Apollysus <142563476+Apollysos@users.noreply.github.com>
2024-01-23 11:49:07 +0100

    added reset button

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let div;
 let input = 0;
 const btn = document.querySelector(".btn");
 btn.textContent = "New Game";
+const btnReset = document.querySelector(".btn-reset");
+btnReset.textContent = "Reset";
+
 for (let i = 0; i < 256; i++) {
   const div = document.createElement("div");
   div.style.border = "2px solid white";
@@ -38,6 +41,26 @@ function deleteDivs() {
 
 function createDivs() {
   for (let i = 0; i < input * input; i++) {
+    const div2 = document.createElement("div");
+    div2.style.border = "2px solid white";
+    div2.style.backgroundColor = "black";
+    div2.style.height = "25px";
+    div2.style.width = "25px";
+    div2.classList.add("div");
+    container.appendChild(div2);
+    div2.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = randomNumberGenerator();
+    });
+  }
+}
+
+btnReset.addEventListener("click", () => {
+  deleteDivs();
+  resetDivs();
+});
+
+function resetDivs() {
+  for (let i = 0; i < 256; i++) {
     const div2 = document.createElement("div");
     div2.style.border = "2px solid white";
     div2.style.backgroundColor = "black";
